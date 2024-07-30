8364c46c648e3d028074820e63ba313ae7c52bbd
Nafypy <nafypy9@gmail.com>
2024-07-30 03:58:16 -0700

    Finally at API

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+"""
+Module to fetch and display TODO list progress of an employee from REST API
+"""
+
+import requests
+
+import sys
+
+
+def get_employee_todo_progress(employee_id):
+    try:
+        employee_id = int(employee_id)
+    except ValueError:
+        print("Invalid employee ID. Enter the correct integer.")
+        return
+    user_url = f"https://jsonplaceholder.typicode.com/users/{employee_id}"
+    user_response = requests.get(user_url)
+    if user_response.status_code != 200:
+        print("Employee not found")
+        return
+    employee_name = user_response.json().get("name")
+    todos_url = f"https://jsonplaceholder.typicode.com/todos?userId={employee_id}"
+    todos_response = requests.get(todos_url)
+    if todos_response.status_code != 200:
+       print("Could not retrieve todo list.")
+       return
+    todos = todos_response.json()
+    #Calculate progress
+    total_tasks = len(todos)
+    done_tasks = [task for task in todos if task.get("completed")]
+    number_of_done_tasks = len(done_tasks)
+    #display results
+    print(f"Employee {employee_name} is done with tasks ({number_of_done_tasks}/{total_tasks}):")
+    for task in done_tasks:
+        print(f"\t {task.get('title')}")
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: ./script_name.py EMPLOYEE_ID")
+    else:
+        get_employee_todo_progress(sys.argv[1])
+       
+
